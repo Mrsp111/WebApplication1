37f985c4b955ba65df7ec3893c8e861333a1d79b
Shivam <shivamabhangi118@outlook.com>
2024-12-23 19:46:37 +0530

    Update Code

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        HELLO WORLD
+        BU       <asp:Button runat="server" Text="Click me" OnClick="Unnamed1_Click"></asp:Button><div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int Num1,Num2,ADD;
+            Num1 = int.Parse(TextBox1.Text);
+            Num2 = int.Parse(TextBox2.Text);
+            ADD=Num1 +Num2;
+
+            Label2.Visible = true;
+            Label2.Text = "Total is:" + ADD;
+        }
+    }
+}
